cc42b92e43f01758a12fc121534d05a208a1f3ca
Bryan DiMaio <bryan.dimaio@gmail.com>
2025-12-11 15:07:26 -0500

    Initial Commit

---
+++ Audiyo/AudiyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiyoApp.swift
+//  Audiyo
+//
+//  Created by Bryan DiMaio on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AudiyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audiyo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Audiyo
+//
+//  Created by Bryan DiMaio on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
